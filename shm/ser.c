72c78dea877c619e542052e82c5d442735db22b5
root <root@localhost.localdomain>
2018-05-03 19:43:56 +0800

    new ser

--- shm/ser.c
+++ shm/ser.c
@@ -24,17 +24,17 @@ int main(int argc,char *argv[])
 	int sem_id=Semget(shm_key,2,IPC_CREAT|IPC_EXCL|0755);
 	union semun init;
 	init.val=0;
-	semctl(sem_id,0,SETVAL,init);
+	semctl(sem_id,0,SETVAL,init);//将信号量集中的0下标的信号量赋值为0
 	semctl(sem_id,1,SETVAL,init);
-	struct sembuf p={0,-1,0};
+	struct sembuf p={0,-1,0};//用0号信号量实现P操作
 	struct sembuf v={1,1,0};
 
 	//4
 	while(1)
 	{
-		printf("Ser:>");
+		printf("Ser:>");//服务器先发起会话
 		scanf("%s",addr);
-		semop(sem_id,&v,1);
+		semop(sem_id,&v,1);//当服务器完成完成数据的输入后，希望客户端能读走数据，对1号信号量进行V操作，此时客户端就可对1号信号量进行P操作，以下P操作类似
 
 		semop(sem_id,&p,1);
 		printf("Cli:>%s\n",addr);
@@ -42,9 +42,9 @@ int main(int argc,char *argv[])
 
 	//5
 	shmdt(addr);
-	semctl(sem_id,IPC_RMID,0);
+	semctl(sem_id,IPC_RMID,0);//删除信号量集上的0号信号量
 	semctl(sem_id,IPC_RMID,1);
 	//6
-	int ret=Shmctl(shm_id,IPC_RMID,NULL);
+	int ret=Shmctl(shm_id,IPC_RMID,NULL);//删除共享内存
 	return 0;
 }
